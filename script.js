1f706d576e0f0b0647dd994e687ebc189cebdcd6
JanaSimjanovska <simjanovska.jana@gmail.com>
2021-02-16 01:29:09 +0100

    login/registration app

--- script.js
+++ script.js
@@ -1,222 +1,142 @@
-let userNumber = document.getElementById("numInput");
-let submitBtn = document.getElementById("submitBtn");
-let resHeading = document.getElementById("resHeading");
+let newUserUsername = document.getElementById("newUserUsername");
+let newUserPassword = document.getElementById("newUserPass");
+let usernameInput = document.getElementById("userName");
+let passwordInput = document.getElementById("pass");
+let registerBtn = document.getElementById("registerBtn");
+let loginBtn = document.getElementById("loginBtn");
+let isAdminBtn = document.getElementById("yes");
+let isNotAdminBtn = document.getElementById("no");
+let alertPar = document.getElementById("alertPar");
+let resPar = document.getElementById("resPar");
+let logOutBtn = document.getElementById("logOut");
+let showIfAdmin = document.getElementById("showIfAdmin");
+let welcomePar = document.getElementById("welcome");
+let usernamesList = document.getElementById("usernamesList");
+
+console.log(resPar, logOutBtn, showIfAdmin, welcomePar, usernamesList, logOutBtn);
+
+logOutBtn.style.display = "none";
 
 
-function convertToWords (number){
-    let toNineteenArray = ["", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"];
-    let tensArray = ["", "", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"];
-    let toTrillionArray = ["", "", "", "hundred", "thousand", "million", "billion", "trillion"];
-
-    if(number.length > 15){
-        return `Please enter a number up to 15 digits!`;
-    }
-    else if(number === ""){
-        return `Please enter a number!`;
-    }
-    else if(parseInt(number) < 0){
-        return `Please enter numbers from 0 and up only!`;
-    } 
-    else if(number.length === 1){
-        if(parseInt(number) === 0){
-            return 'zero';
-        }
-        else {
-            return `${toNineteenArray[number]}`;
-        }
-    }
-    else if(number.length === 2){
-        if(parseInt(number[0]) === 0){
-            if(parseInt(number[1]) === 0){
-                return 'zero';
-            }
-            else{
-                return `${toNineteenArray[number[1]]}`;
-            }
-        }
-        else if(parseInt(number) < 20){
-            return `${toNineteenArray[number]}`;
-        }
-        else {
-            if(parseInt(number[1]) === 0){
-                return `${tensArray[number[0]]}`;
-            } else {
-                return `${tensArray[number[0]]}-${toNineteenArray[number[1]]}`;
-            }
-        }
-    }
-    else if(number.length === 3){
-        if(parseInt(number[0]) === 0){
-            return `${convertToWords(number.slice(1, 3))}`;
-        }
-        else {
-            if(parseInt(number[1]) === 0 && parseInt(number[2]) === 0){
-                return `${toNineteenArray[number[0]]} ${toTrillionArray[3]}`;
-            }
-            else {
-                return `${toNineteenArray[number[0]]} ${toTrillionArray[3]} ${convertToWords(number.slice(1, 3))}`;
-            }
-        }
-    }
-    else if(number.length === 4){
-        if(parseInt(number[0]) === 0){
-            return `${convertToWords(number.slice(1, 4))}`;
-        }
-        else {
-            if(parseInt(number[1]) === 0 && parseInt(number[2]) === 0 && parseInt(number[3]) === 0){
-                return `${toNineteenArray[number[0]]} ${toTrillionArray[4]}`;
-            }
-            else {
-                return `${toNineteenArray[number[0]]} ${toTrillionArray[4]} ${convertToWords(number.slice(1, 4))}`;
-            }
-        }
-    }
-    else if(number.length === 5){
-        if(parseInt(number[0]) === 0){
-            return `${convertToWords(number.slice(1, 5))}`;
-        }
-        else {
-            if(parseInt(number[2]) === 0 && parseInt(number[3]) === 0 && parseInt(number[4]) === 0){
-                return `${convertToWords(number.slice(0, 2))} ${toTrillionArray[4]}`;
-            }
-            else{
-                return `${convertToWords(number.slice(0, 2))} ${toTrillionArray[4]} ${convertToWords(number.slice(2, 5))}`;
-            }
-        }
-    }
-    else if(number.length === 6){
-        if(parseInt(number[0]) === 0){
-            return `${convertToWords(number.slice(1, 6))}`;
-        }
-        else {
-            if(parseInt(number[3]) === 0 && parseInt(number[4]) === 0 && parseInt(number[5]) === 0){
-                return `${convertToWords(number.slice(0, 3))} ${toTrillionArray[4]}`;
-            }
-            else{
-                return `${convertToWords(number.slice(0, 3))} ${toTrillionArray[4]} ${convertToWords(number.slice(3, 6))}`;
-            }
-        }
-    }
-    else if(number.length === 7){
-        if(parseInt(number[0]) === 0){
-            return `${convertToWords(number.slice(1, 7))}`;
-        }
-        else {
-            if(parseInt(number[1]) === 0 && parseInt(number[2]) === 0 && parseInt(number[3]) === 0 && parseInt(number[4]) === 0 && parseInt(number[5]) === 0 && parseInt(number[6]) === 0){
-                return `${convertToWords(number.slice(0, 1))} ${toTrillionArray[5]}`;
-            }
-            else {
-                return `${convertToWords(number.slice(0, 1))} ${toTrillionArray[5]} ${convertToWords(number.slice(1, 7))}`;
-            } 
-        }
-    }
-    else if(number.length === 8){
-        if(parseInt(number[0]) === 0){
-            return `${convertToWords(number.slice(1, 8))}`;
-        }
-        else {
-            if(parseInt(number[2]) === 0 && parseInt(number[3]) === 0 && parseInt(number[4]) === 0 && parseInt(number[5]) === 0 && parseInt(number[6]) === 0 && parseInt(number[7]) === 0){
-                return `${convertToWords(number.slice(0, 2))} ${toTrillionArray[5]}`;
-            }
-            else {
-            return `${convertToWords(number.slice(0, 2))} ${toTrillionArray[5]} ${convertToWords(number.slice(2, 8))}`;
-            }
-        }
-    }
-    else if(number.length === 9){
-        if(parseInt(number[0]) === 0){
-            return `${convertToWords(number.slice(1, 9))}`;
-        }
-        else {
-            if(parseInt(number[3]) === 0 && parseInt(number[4]) === 0 && parseInt(number[5]) === 0 && parseInt(number[6]) === 0 && parseInt(number[7]) === 0 && parseInt(number[8]) === 0){
-                return `${convertToWords(number.slice(0, 3))} ${toTrillionArray[5]}`;
-            }
-            else {
-                return `${convertToWords(number.slice(0, 3))} ${toTrillionArray[5]} ${convertToWords(number.slice(3, 9))}`;
-            }
-        }
-    }
-    else if(number.length === 10){
-        if(parseInt(number[0]) === 0){
-            return `${convertToWords(number.slice(1, 10))}`;
-        }
-        else {
-            if(parseInt(number[1]) === 0 && parseInt(number[2]) === 0 && parseInt(number[3]) === 0 && parseInt(number[4]) === 0 && parseInt(number[5]) === 0 && parseInt(number[6]) === 0 && parseInt(number[7]) === 0 && parseInt(number[8]) === 0 && parseInt(number[9]) === 0){
-                return `${convertToWords(number.slice(0, 1))} ${toTrillionArray[6]}`;
-            }
-            else {
-                return `${convertToWords(number.slice(0, 1))} ${toTrillionArray[6]} ${convertToWords(number.slice(1, 10))}`;
-            }
-        }
-    }
-    else if(number.length === 11){
-        if(parseInt(number[0]) === 0){
-            return `${convertToWords(number.slice(1, 11))}`;
-        }
-        else {
-            if(parseInt(number[2]) === 0 && parseInt(number[3]) === 0 && parseInt(number[4]) === 0 && parseInt(number[5]) === 0 && parseInt(number[6]) === 0 && parseInt(number[7]) === 0 && parseInt(number[8]) === 0 && parseInt(number[9]) === 0 && parseInt(number[10]) === 0){
-                return `${convertToWords(number.slice(0, 2))} ${toTrillionArray[6]}`;
-            }
-            else {
-                return `${convertToWords(number.slice(0, 2))} ${toTrillionArray[6]} ${convertToWords(number.slice(2, 11))}`;
-            }
-        }
-    }
-    else if(number.length === 12){
-        if(parseInt(number[0]) === 0){
-            return `${convertToWords(number.slice(1, 12))}`;
-        }
-        else {
-            if(parseInt(number[3]) === 0 && parseInt(number[4]) === 0 && parseInt(number[5]) === 0 && parseInt(number[6]) === 0 && parseInt(number[7]) === 0 && parseInt(number[8]) === 0 && parseInt(number[9]) === 0 && parseInt(number[10]) === 0 && parseInt(number[11]) === 0){
-                return `${convertToWords(number.slice(0, 3))} ${toTrillionArray[6]}`;
-            }
-            else {
-                return `${convertToWords(number.slice(0, 3))} ${toTrillionArray[6]} ${convertToWords(number.slice(3, 12))}`;
-            }
-        }
-    }
-    else if(number.length === 13){
-        if(parseInt(number[0]) === 0){
-            return `${convertToWords(number.slice(1, 13))}`;
-        }
-        else {
-            if(parseInt(number[1]) === 0 && parseInt(number[2]) === 0 && parseInt(number[3]) === 0 && parseInt(number[4]) === 0 && parseInt(number[5]) === 0 && parseInt(number[6]) === 0 && parseInt(number[7]) === 0 && parseInt(number[8]) === 0 && parseInt(number[9]) === 0 && parseInt(number[10]) === 0 && parseInt(number[11]) === 0 && parseInt(number[3]) === 0){
-                return `${convertToWords(number.slice(0, 1))} ${toTrillionArray[7]}`;
-            }
-            else {
-                return `${convertToWords(number.slice(0, 1))} ${toTrillionArray[7]} ${convertToWords(number.slice(1, 13))}`;
-            }
-        }
-    }
-    else if(number.length === 14){
-        if(parseInt(number[0]) === 0){
-            return `${convertToWords(number.slice(1, 14))}`;
-        }
-        else {
-            if(parseInt(parseInt(number[2]) === 0 && parseInt(number[3]) === 0 && parseInt(number[4]) === 0 && parseInt(number[5]) === 0 && parseInt(number[6]) === 0 && parseInt(number[7]) === 0 && parseInt(number[8]) === 0 && parseInt(number[9]) === 0 && parseInt(number[10]) === 0 && parseInt(number[11]) === 0 && parseInt(number[12]) === 0 && number[13]) === 0){
-                return `${convertToWords(number.slice(0, 2))} ${toTrillionArray[7]}`;
-            }
-            else {
-                return `${convertToWords(number.slice(0, 2))} ${toTrillionArray[7]} ${convertToWords(number.slice(2, 14))}`;
-            }
-        }
-    }
-    else if(number.length === 15){
-        if(parseInt(number[0]) === 0){
-            return `${convertToWords(number.slice(1, 15))}`;
-        }
-        else {
-            if(parseInt(parseInt(number[3]) === 0 && parseInt(number[4]) === 0 && parseInt(number[5]) === 0 && parseInt(number[6]) === 0 && parseInt(number[7]) === 0 && parseInt(number[8]) === 0 && parseInt(number[9]) === 0 && parseInt(number[10]) === 0 && parseInt(number[11]) === 0 && parseInt(number[12]) === 0 && number[13]) === 0 && parseInt(number[2]) === 0){
-                return `${convertToWords(number.slice(0, 3))} ${toTrillionArray[7]}`;
-            }
-            else {
-                return `${convertToWords(number.slice(0, 3))} ${toTrillionArray[7]} ${convertToWords(number.slice(3, 15))}`;
-            }
-        }
-    }
-}    
+function User (username, password, isAdmin) {
+    this.username = username;
+    this.pass = password;
+    this.isAdmin = isAdmin;
+}
 
 
-submitBtn.addEventListener("click", function(){
-    resHeading.innerText = `${convertToWords(userNumber.value)}`;
+let admin = new User("JanaS", "jana123", true);
+console.log(typeof(admin.username));
+let testUser = new User("NadjaI", "nadja123", false);
+let users = [testUser, admin];
+
+
+function createUser(username, pass){
+    if(username === "" || pass === ""){
+        alertPar.innerText = `Username and password fields can't be empty!`
+        document.getElementById("usernamesList").innerHTML = "";
+    }
+    else if(username.length < 4){
+        alertPar.innerText = `Username must contain at least 4 characters!`
+        document.getElementById("usernamesList").innerHTML = "";
+    }
+    else if(pass.length < 6){
+        alertPar.innerText = `Password must contain at least 6 characters!`
+        document.getElementById("usernamesList").innerHTML = "";
+    }
+    else {
+        document.getElementById("usernamesList").innerText = "";
+        users.push(new User(username, pass, false));
+        welcomePar.innerText = `Welcome ${username}! Go ahead and login with the username and password you created!`
+        for(let i = 0; i < users.length; i++){
+            usernamesList.innerHTML += 
+            `<li>${users[i].username}</li>`
+        }
+        alertPar.innerText = ""; 
+    }
+}
+
+
+let usersIndex = "";
+function checkIfExists(username, pass, array){
+    usersIndex = array.findIndex(x => x.username === username && x.pass === pass);
+}
+
+
+function checkStatus(array, username, pass){
+    if(username.length === 0 || pass.length === 0){
+        resPar.innerText = `Username and password fields can't be empty!`
+    }
+    else {
+        if(usersIndex === -1){
+            resPar.innerText = `User doesn't exist! Try again!`;
+            showIfAdmin.innerHTML = "";
+            logOutBtn.style.display = "none";
+        }
+        else {
+            if(array[usersIndex].isAdmin === true){
+                for(let j = 0; j < array.length; j++){
+                    resPar.innerText = `Welcome Admin ${username}!`;
+                    showIfAdmin.innerHTML += `<li>Username: ${array[j].username}; Password: ${array[j].pass}; isAdmin: ${array[j].isAdmin};</li>` 
+                }
+            }
+            else if(array[usersIndex].isAdmin === false) {
+                showIfAdmin.innerHTML = "";
+                resPar.innerText = `Welcome ${username}!`;
+            }
+            logOutBtn.style.display = "block";
+        }
+    }  
+}
+
+
+logOutBtn.addEventListener("click", function(){
+    resPar.innerText = "";
+    showIfAdmin.innerHTML = "";
+    logOutBtn.style.display = "none";
+
+})
+
+
+registerBtn.addEventListener("click", function(){
+    alertPar.innerText = "";
+    welcomePar.innerText = "";
+    resPar.innerText = "";
+    showIfAdmin.innerHTML = "";
+    logOutBtn.style.display = "none";
+    createUser(newUserUsername.value, newUserPassword.value);
+    document.getElementsByClassName("user-img")[1].style.display = "initial";
+    document.getElementsByClassName("pass-img")[1].style.display = "initial";
+    newUserUsername.value = "";
+    newUserPassword.value = "";
+})
+
+
+loginBtn.addEventListener("click", function(){
+    welcomePar.innerText = "";
+    usernamesList.innerText = "";
+    alertPar.innerText = "";
+    checkIfExists(usernameInput.value, passwordInput.value, users);
+    checkStatus(users, usernameInput.value, passwordInput.value);
+    document.getElementsByClassName("user-img")[0].style.display = "initial";
+    document.getElementsByClassName("pass-img")[0].style.display = "initial";
+    usernameInput.value = "";
+    passwordInput.value = "";
+})
+
+
+document.getElementById("userName").addEventListener("keypress", function(){
+    document.getElementsByClassName("user-img")[0].style.display = "none";
+})
+
+document.getElementById("pass").addEventListener("keypress", function(){
+    document.getElementsByClassName("pass-img")[0].style.display = "none";
+})
+
+document.getElementById("newUserUsername").addEventListener("keypress", function(){
+    document.getElementsByClassName("user-img")[1].style.display = "none";
+})
+
+document.getElementById("newUserPass").addEventListener("keypress", function(){
+    document.getElementsByClassName("pass-img")[1].style.display = "none";
 })
